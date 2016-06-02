d65265ddae1d7f3e9c169e3183237ec8bae1bc1d
Robert Zuba <robert.szymon.zuba@gmail.com>
2016-06-02 01:01:36 +0100

    Troche zmian, przed wprowazdeniem Bus

--- app/src/main/java/robin/com/weather/MainActivity.java
+++ app/src/main/java/robin/com/weather/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,79 +16,57 @@ import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.Retrofit;
 import robin.com.weather.api.LocationService;
 import robin.com.weather.api.RetrofitService;
-import robin.com.weather.api.WeatherService;
+import robin.com.weather.controller.MainController;
 import robin.com.weather.model.location.Location;
-import robin.com.weather.model.weather.Weather;
 
 public class MainActivity extends AppCompatActivity {
+    Retrofit retrofit = RetrofitService.getInstance().getRetrofit();
+    boolean isFindedLocation;
     @BindView(R.id.editTextCity) EditText city;
     @BindView(R.id.textView2) TextView textViewTemp;
     @BindView(R.id.textView3) TextView textViewWeather;
-    @BindView(R.id.textView4) TextView country;
+    @BindView(R.id.textView4) TextView textViewCity;
     @BindView(R.id.buttonSubmit) Button submit;
+    @BindView(R.id.imageViewWeatherIcon) ImageView imageView;
+    MainController controller = new MainController();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        getWeather("265535");
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
-
-                final LocationService locationService = RetrofitService.getInstance().getRetrofit().create(LocationService.class);
-                Call<List<Location>> callLocation = locationService.findLocation(city.getText().toString());
-                callLocation.enqueue(new Callback<List<Location>>() {
-                    private Location location;
-
-                    @Override
-                    public void onResponse(Call<List<Location>> call, Response<List<Location>> response) {
-                        location = response.body().get(0);
-
-                        setCountry(location.country.localizedName);
-                        getWeather(location.key);
-
-
-
-
-
-                    }
-
-                    @Override
-                    public void onFailure(Call<List<Location>> call, Throwable t) {
-                        Toast.makeText(getApplicationContext(),"Nie znaleziono miasta ",Toast.LENGTH_SHORT).show();
-                    }
-                });
+                controller.localization(city.getText().toString(),textViewCity);
 
             }
         });
     }
-    private void setCountry ( String name) {
-        country.setText(name);
-    }
-    private void setTemperature(String temperature) {
-        textViewTemp.setText(temperature);
-    }
-    private void setWeatherText(String weatherText) {
-        textViewWeather.setText(weatherText);
-    }
-    private void getWeather(String key) {
-        WeatherService weatherService = RetrofitService.getInstance().getRetrofit().create(WeatherService.class);
-        Call<List<Weather>> callWeather = weatherService.getWeather(key);
-        callWeather.enqueue(new Callback<List<Weather>>() {
+
+    private void findLocation(String location) {
+        Call<List<Location>> call = retrofit.create(LocationService.class).findLocation(location);
+        call.enqueue(new Callback<List<Location>>() {
+            Location location;
             @Override
-            public void onResponse(Call<List<Weather>> call, Response<List<Weather>> response) {
-                Weather w = response.body().get(0);
-                setTemperature(w.getTemperature().getMetric().getValue().toString());
-                setWeatherText(w.getWeatherText());
+            public void onResponse(Call<List<Location>> call, Response<List<Location>> response) {
+                if(response.body().isEmpty()) {
+                    isFindedLocation = false;
+                    Toast.makeText(getApplicationContext(),"Nie znaleziono miasta", Toast.LENGTH_SHORT).show();
+                } else {
+                    isFindedLocation = true;
+                    location = response.body().get(0);
+                }
+                textViewCity.setText(location.geoPosition.latitude+" | "+location.geoPosition.longitude);
+                textViewTemp.setText(response.body().size());
             }
 
             @Override
-            public void onFailure(Call<List<Weather>> call, Throwable t) {
-
+            public void onFailure(Call<List<Location>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(),"Błąd rzadania", Toast.LENGTH_SHORT).show();
             }
         });
     }
